341c75447a0c488df564436f854451966473ceb4
andreaurist <andreaurist@yahoo.com>
2013-11-05 11:03:17 -0500

    HTML5 audio test
    audio hosted on Amazon S3

---
+++ The Surrey_files/playsound.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var aud = document.createElement("iframe");
+aud.setAttribute('src', "/The Surrey_files/jazzloop.mp3"); // replace with actual file path
+aud.setAttribute('width', '1px');
+aud.setAttribute('height', '1px');
+aud.setAttribute('scrolling', 'no');
+aud.style.border = "0px";
+document.body.appendChild(aud);
